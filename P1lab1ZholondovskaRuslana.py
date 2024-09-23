523d2f6052347d5a90a0284fb18838a334d39194
RuslanaZholondovska <ruslanagoode@gmail.com>
2024-09-22 17:21:52 -0700

    Add files via upload

---
+++ P1lab1ZholondovskaRuslana.py
@@ -0,0 +1,12 @@
+# Ruslana Zholondovska
+# 9/22/24
+# P1LAB1
+# Task to welcome users by their personal names
+
+
+first_name = input("enter your first name: ")
+
+last_name = input("enter your last name: ")
+
+print("Hello, " + first_name + " " + last_name + ",! Welcome to CTI-110")
+
